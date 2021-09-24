360431eed0bd2251b148f198300742afa795d766
zenoxide <12200015@bsi.ac.id>
2021-09-25 06:11:35 +0700

    update v_header.php

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -35,7 +35,7 @@
       <div class="container">
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
-            <li class="nav-item"><a class="nav-link" href="<?php echo base_url().'index.php/web' ?>">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="<?php echo base_url().'web' ?>">Home</a></li>
           </ul>
         </div>
       </div>
